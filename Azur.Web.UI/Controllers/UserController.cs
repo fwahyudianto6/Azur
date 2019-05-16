3054a0da16cd762ce684bca5abac7859083b79b2
fwahyudianto06 <fwahyudi06@gmail.com>
2019-05-17 00:08:06 +0700

    Add User Controller Class

---
+++ Azur.Web.UI/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+#region Trademark
+
+/**
+ *  This software, all associated documentation, and all copies are CONFIDENTIAL INFORMATION of Kalpawreska Teknologi Indonesia
+ *  http://www.fwahyudianto.id
+ *  ® Wahyudianto, Fajar
+ *  Email 	: fwahyudi06@gmail.com
+ */
+
+#endregion // end of Trademark
+
+namespace Azur.Web.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        #region Method
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        #endregion // end of Method 
+    }
+}
